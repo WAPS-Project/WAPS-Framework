f1647d59c959aa7295c4aa0f3d1b15ad97821d98
JosunLP <jolun.swtor@gmail.com>
2018-12-05 22:42:22 +0100

    working on sizes, adding fav.ico and dummy logo

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <i class="fab fa-accusoft logo"></i>
+  <img src="img/fav.svg" alt="logo" class="logo">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>

4e983fce4079161fcf7d53ba4606c0c07440a2f4
JosunLP <jolun.swtor@gmail.com>
2018-12-06 22:38:54 +0100

    working on mobile design and menue

--- header.php
+++ header.php
@@ -1,34 +1,24 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <img src="img/fav.svg" alt="logo" class="logo">
   <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+  <button class="navbar-toggler navbar-toggler-icon" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"></button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      <li class="nav-item">
+        <?php $indexname1 = "Home"; ?>
+        <a class="nav-link <?php if ($pagename == $indexname1) {echo 'active';}?> " href="index.php">Home <?php if ($pagename == $indexname1) {echo "<span class='sr-only'>(current)</span>";}?></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
+        <?php $indexname2 = "Shop"; ?>
+        <a class="nav-link <?php if ($pagename == $indexname2) {echo 'active';}?>" href="shop.php">Shop <?php if ($pagename == $indexname2) {echo "<span class='sr-only'>(current)</span>";}?></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+        <?php $indexname3 = "Disabled"; ?>
+        <a class="nav-link disabled <?php if ($pagename == $indexname3) {echo 'active';}?>" href="#">Disabled <?php if ($pagename == $indexname3) {echo "<span class='sr-only'>(current)</span>";}?></a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
+    <form class="form-inline my-2 my-lg-0 search">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
---
+++ shop.php
@@ -0,0 +1,36 @@
+<?php
+require 'tools.php';
+$pagename = "Shop";//#####Here you define your Page Name######
+
+?>
+<!DOCTYPE html>
+<html lang="de" dir="ltr">
+
+<?php
+
+include 'head.php';
+
+?>
+<body>
+<?php
+
+include 'header.php';
+?>
+
+<div class="content">
+
+    <h1 class="title"><?php echo $pagename ?></h1>
+
+</div>
+
+<?php
+include 'footer.php';
+?>
+</body>
+
+
+
+
+
+
+</html>

3bbd2e172466ab2802e16d781def8796a1f9f8b8
j.pfalzgraf <j.pfalzgraf@ccvossel.de>
2019-09-27 07:51:38 +0200

    adding Error Types

--- class/ErrorHandler.class.php
+++ class/ErrorHandler.class.php
@@ -6,8 +6,23 @@ class ErrorHandler
 
     const FOOTER = '<span>For help ask at <a href="https://gitlab.com/JosunLP/webapp_php_sample">https://gitlab.com/JosunLP/webapp_php_sample</a></span>';
 
-    function CreateError($type, $message)
+    function FireError($type, $message)
     {
         echo "<script>Swal.fire({type: 'error', title: '$type', text: '$message', showCloseButton: true, footer: '" . self::FOOTER . "'})</script>";
+    }
+
+    function FireWarning($type, $message)
+    {
+        echo "<script>Swal.fire({type: 'warning', title: '$type', text: '$message', showCloseButton: true, footer: '" . self::FOOTER . "', animation: true})</script>";
+    }
+
+    function CreateError($type, $message, $weight, $isFatal)
+    {
+        if ($weight >= 3 && $isFatal) {
+            throw new Exception("<script>Swal.fire({type: 'error', title: '$type', text: '$message . This is a fatal Error!', showCloseButton: true, footer: '" . self::FOOTER . "'})</script>");
+        }
+        elseif ($weight <= 3 && !$isFatal) {
+            echo "<script>Swal.fire({type: 'error', title: '$type', text: '$message', showCloseButton: true, footer: '" . self::FOOTER . "'})</script>";
+        }
     }
 }

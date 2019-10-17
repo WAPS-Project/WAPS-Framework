3dd855237b8a9c68f5f3bce2ae0e5a285ef70b9f
j.pfalzgraf <j.pfalzgraf@ccvossel.de>
2019-10-17 13:54:09 +0200

    adding npm and js parsing over tsc

--- core/head.php
+++ core/head.php
@@ -17,6 +17,6 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
           integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <!-- Adding Font Awesome -->
-    <link rel="stylesheet" href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/content/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?php $_SERVER['DOCUMENT_ROOT'] ?>/content/css/main.css"> <!-- Adding Main CSS -->
+    <link rel="stylesheet" href="/content/css/bootstrap.css">
+    <link rel="stylesheet" href="/content/css/main.css"> <!-- Adding Main CSS -->
 </head>

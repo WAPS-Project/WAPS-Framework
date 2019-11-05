3cbb92bec2990996d6d064e57bb30d31837f41fa
j.pfalzgraf <j.pfalzgraf@ccvossel.de>
2019-11-05 14:05:51 +0100

    fixing mail adress constructor

--- class/Mailer.class.php
+++ class/Mailer.class.php
@@ -12,17 +12,17 @@ class Mailer
 
         switch ($mode){
             case MAIL_INFO: {
-                $header .= "From: " . MAIL_INFO . $_SERVER['HTTP_HOST'] . "\r\n";
-                $header .= "Reply-To: " . MAIL_INFO . $_SERVER['HTTP_HOST'] . "\r\n";
+                $header .= "From: " . MAIL_INFO . "@" .  $_SERVER['HTTP_HOST'] . "\r\n";
+                $header .= "Reply-To: " . MAIL_INFO . "@" . $_SERVER['HTTP_HOST'] . "\r\n";
             }
 
             case MAIL_AUTO: {
-                $header .= "From: " . MAIL_AUTO . $_SERVER['HTTP_HOST'] . "\r\n";
+                $header .= "From: " . MAIL_AUTO . "@" . $_SERVER['HTTP_HOST'] . "\r\n";
             }
 
             case MAIL_SUPPORT: {
-                $header .= "From: " . MAIL_SUPPORT . $_SERVER['HTTP_HOST'] . "\r\n";
-                $header .= "Reply-To: " . MAIL_SUPPORT . $_SERVER['HTTP_HOST'] . "\r\n";
+                $header .= "From: " . MAIL_SUPPORT . "@" . $_SERVER['HTTP_HOST'] . "\r\n";
+                $header .= "Reply-To: " . MAIL_SUPPORT . "@" . $_SERVER['HTTP_HOST'] . "\r\n";
             }
         }
 

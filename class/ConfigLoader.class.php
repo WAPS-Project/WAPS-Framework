f6fc82ba7570ee7fcd65bdaf203c3ef35898dba0
Jonas Pfalzgraf <jolun.swtor@gmail.com>
2019-11-16 19:10:09 +0100

    reworking config loader

--- class/ConfigLoader.class.php
+++ class/ConfigLoader.class.php
@@ -4,11 +4,23 @@
 namespace webapp_php_sample_class;
 
 
-use RecursiveArrayIterator;
-use RecursiveIteratorIterator;
-
 class ConfigLoader
 {
+    public static function loadConfig($path)
+    {
+        $config = self::validateConfig($path);
+
+        if ($config != false) {
+
+            foreach ($config as $firstLevel) {
+                foreach ($firstLevel as $key => $value) {
+                    define(strtoupper($key), $value);
+                }
+            }
+            error_reporting(E_ALL);
+        }
+    }
+
     private static function validateConfig($path)
     {
         $files = scandir($path);
@@ -26,49 +38,5 @@ class ConfigLoader
             }
         }
         return false;
-    }
-
-    public static function loadConfig($path) {
-        $config = self::validateConfig($path);
-
-        if ($config != false) {
-            $charset = $config["metaData"]["charset"];
-            $language = $config["metaData"]["language"];
-            $description = $config["metaData"]["description"];
-            $keywords = $config["metaData"]["keywords"];
-            $author = $config["metaData"]["author"];
-            $pageTitle = $config["metaData"]["pageTitle"];
-
-            $DBHOST = $config["database"]["dbhost"];
-            $DBUSER = $config["database"]["dbuser"];
-            $DBPASSWORD = $config["database"]["dbpassword"];
-            $DBNAME = $config["database"]["dbname"];
-
-            $version = $config["head"]["version"];
-
-            $infoMail = $config["mailConfig"]["infoMail"];
-            $autoMail = $config["mailConfig"]["autoMail"];
-            $supportMail = $config["mailConfig"]["supportMail"];
-
-            define("CHARSET", $charset);
-            define("LANGUAGE", $language);
-            define("DESCRIPTION", $description);
-            define("KEYWORDS", $keywords);
-            define("AUTHOR", $author);
-            define("PAGE_TITLE",$pageTitle);
-            define('MYSQL_HOST', $DBHOST);
-            define('MYSQL_USER', $DBUSER);
-            define('MYSQL_KEYWORD', $DBPASSWORD);
-            define('MYSQL_DATABASE', $DBNAME);
-            define('VERSION', $version);
-            define('MAIL_INFO', $infoMail);
-            define('MAIL_AUTO', $autoMail);
-            define('MAIL_SUPPORT', $supportMail);
-
-            error_reporting(E_ALL);
-
-
-
-        }
     }
 }

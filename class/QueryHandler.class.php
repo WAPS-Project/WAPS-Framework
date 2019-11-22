74bbbd6b240188a6059e0ba2cb997fce65d3e28b
j.pfalzgraf <j.pfalzgraf@ccvossel.de>
2019-11-22 16:04:47 +0100

    init validation class

--- class/QueryHandler.class.php
+++ class/QueryHandler.class.php
@@ -6,5 +6,13 @@ namespace webapp_php_sample_class;
 
 class QueryHandler
 {
+    public static function validateQuery($queryString) {
 
+    }
+
+    protected static function filterQuery($queryString) {
+        $querySplit = explode("--", $queryString);
+        $queryJoin = implode(" ", $querySplit);
+        $querySplit = explode(" ", $queryJoin);
+    }
 }

eebddf19ca510335855e4e5ebcbdcbc38c0d2f58
JosunLP <jolun.swtor@gmail.com>
2019-12-17 22:22:50 +0100

    adding basic API

---
+++ API.php
@@ -0,0 +1,21 @@
+<?php
+
+use webapp_php_sample_class\JsonHandler;
+use webapp_php_sample_class\Main;
+
+include 'core/API.loader.php';
+
+$APIString = "core/api/";
+
+$APIFiles = array_diff(scandir($APIString), array('.', '..'));
+$command = Main::checkGet("apiMode");
+
+foreach ($APIFiles as $singleAPI) {
+    $fileParts = explode(".", $singleAPI);
+    if ($command === $fileParts[0]) {
+        include $APIString . $singleAPI;
+    }
+    elseif ($command === null) {
+        JsonHandler::FireSimpleJson("No content warning", "Your request contains no valid Data");
+    }
+}

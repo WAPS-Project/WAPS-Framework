5c598bc494e1924693b62782c92fb624db76cf67
JosunLP <jolun.swtor@gmail.com>
2019-12-18 18:42:34 +0100

    working on migrations

--- class/Migration.class.php
+++ class/Migration.class.php
@@ -6,5 +6,42 @@ namespace webapp_php_sample_class;
 
 class Migration
 {
+    const MIGRATION_PATH = "./core/database/migrations/";
 
+    public function migrate()
+    {
+        self::loadMigrations();
+    }
+
+    public function createMigration($migrationName, $query)
+    {
+        $migrationPreset = date("YmdHis");
+        $migrationFileName = $migrationPreset . "_" . $migrationName . ".php";
+        $migrationClassName = $migrationPreset . "_" . $migrationName;
+
+        $migrationHead = "
+        <?php
+        
+        namespace webapp_php_sample_migration;
+        
+        class " . $migrationClassName .
+            "
+        
+        ";
+
+
+    }
+
+    private function loadMigrations()
+    {
+        $files = array_diff(scandir(self::MIGRATION_PATH), DEFAULT_FILE_FILTER);
+        foreach ($files as $file) {
+            include self::MIGRATION_PATH . $file;
+        }
+    }
+
+    private function generateSql($query)
+    {
+
+    }
 }

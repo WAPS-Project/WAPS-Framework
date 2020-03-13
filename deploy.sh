479ff3a75eda7e61602ef82bd906d9621e3e9371
JosunLP <jolun.swtor@gmail.com>
2020-03-13 19:17:13 +0100

    adding alternative deploy script

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+rm -rf ./framework.dist
+mkdir ./framework.dist
+cp -rf ./framework.src/class ./framework.dist/class
+cp -rf ./framework.src/config ./framework.dist/config
+cp -rf ./framework.src/content ./framework.dist/content
+cp -rf ./framework.src/core ./framework.dist/core
+cp -rf ./framework.src/custom ./framework.dist/custom
+cp -rf ./framework.src/object ./framework.dist/object
+cp -rf ./framework.src/page ./framework.dist/page
+cp -rf ./framework.src/API.php ./framework.dist/API.php
+cp -rf ./framework.src/CLI.php ./framework.dist/CLI.php
+cp -rf ./framework.src/index.php ./framework.dist/index.php
+cp -rf ./framework.src/robots.txt ./framework.dist/robots.txt
+cp -rf ./framework.src/.htaccess ./framework.dist/.htaccess

39116a2e56da0b794e6b81917c3d717a2b75a7dd
JosunLP <jolun.swtor@gmail.com>
2020-03-13 22:45:58 +0100

    creating class

---
+++ framework.src/class/DatatypeConverter.class.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace webapp_php_sample_class;
+
+
+class DatatypeConverter
+{
+    public static function xml2Json($contentString) {
+
+    }
+
+    public static function json2Xml($contentString) {
+
+    }
+
+}

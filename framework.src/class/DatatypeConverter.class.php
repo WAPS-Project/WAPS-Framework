657ba6b629e0e1ef108f3803d69e6d42e3b6d92e
JosunLP <jolun.swtor@gmail.com>
2020-03-14 19:14:03 +0100

    fast Converter fix

--- framework.src/class/DatatypeConverter.class.php
+++ framework.src/class/DatatypeConverter.class.php
@@ -1,54 +1,13 @@
 <?php
 
-// Copyright: Maurits van der Schee <maurits@vdschee.nl>
-// Description: Convert from JSON to XML and back.
-// License: MIT
-
 namespace webapp_php_sample_class;
-
-
 
 use DOMDocument;
 
 class DatatypeConverter
 {
     public static function xml2json($xml) {
-        $a = dom_import_simplexml(simplexml_load_string($xml));
-        $t = function($v) {
-            return $v->getAttribute('type');
-        };
-        $f = function($f,$a) use ($t) {
-            $c = null;
-            if ($t($a)=='null') {
-                $c = null;
-            } else if ($t($a)=='boolean') {
-                $b = substr(strtolower($a->textContent),0,1);
-                $c = in_array($b,array('1','t'));
-            } else if ($t($a)=='number') {
-                $c = $a->textContent+0;
-            } else if ($t($a)=='string') {
-                $c = $a->textContent;
-            } else if ($t($a)=='object') {
-                $c = array();
-                if ($a->getAttribute('__type')) {
-                    $c['__type'] = $a->getAttribute('__type');
-                }
-                for ($i=0;$i<$a->childNodes->length;$i++) {
-                    $v = $a->childNodes[$i];
-                    $c[$v->nodeName] = $f($f,$v);
-                }
-                $c = (object)$c;
-            } else if ($t($a)=='array') {
-                $c = array();
-                for ($i=0;$i<$a->childNodes->length;$i++) {
-                    $v = $a->childNodes[$i];
-                    $c[$i] = $f($f,$v);
-                }
-            }
-            return $c;
-        };
-        $c = $f($f,$a);
-        return json_encode($c,64);
+        return json_encode(simplexml_load_string($xml));
     }
 
     public static function json2xml($json) {

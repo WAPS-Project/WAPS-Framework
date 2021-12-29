6329349d1df2c2685b334829ddff62b8cd250682
Jonas Pfalzgraf <jolun.swtor@gmail.com>
2021-12-29 15:43:10 +0100

    working on new deploy and file watching

--- tools/deploy.php
+++ tools/deploy.php
@@ -1,12 +1,52 @@
 <?php
 
-$DEPLOY_TARGET = '"C:/xampp/htdocs"';
+// check OS
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+	$os = 'win';
+} else {
+	$os = 'nix';
+}
+
+// check if os is mac
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'MAC') {
+	$os = 'mac';
+}
+
+// check PHP version
+if (version_compare(PHP_VERSION, '5.3.0', '<')) {
+	die('PHP 5.3.0 or higher is required!');
+}
+
+// get the name of the OS
+if ($os == 'win') {
+	$os_name = 'windows';
+} elseif ($os == 'mac') {
+	$os_name = 'mac';
+} else {
+	$os_name = 'linux';
+}
+
+$DEPLOY_TARGET = '"./framework.dist"';
 // C:/xampp/htdocs
 // ./framework.dist
 
-system("rd /s /q $DEPLOY_TARGET");
+echo "Identifyed OS: $os_name\n";
+echo "Deploying to $DEPLOY_TARGET\n";
+switch ($os) {
+	case 'win':
+		system("rd /s /q $DEPLOY_TARGET");
 
-system("mkdir $DEPLOY_TARGET");
+		system("mkdir $DEPLOY_TARGET");
 
-echo "Copy folder contents of framework.src to deploy target: $DEPLOY_TARGET\n";
-system("xcopy framework.src $DEPLOY_TARGET /s /e /y");
+		echo "Copy folder contents of framework.src to deploy target: $DEPLOY_TARGET\n";
+		system("xcopy framework.src $DEPLOY_TARGET /s /e /y");
+		break;
+	case 'nix':
+		system("rm -rf $DEPLOY_TARGET");
+
+		system("mkdir $DEPLOY_TARGET");
+
+		echo "Copy folder contents of framework.src to deploy target: $DEPLOY_TARGET\n";
+		system("cp -r framework.src $DEPLOY_TARGET");
+		break;
+}
---
+++ tools/watcher.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const path = require('path');
+const src = './framework.src/';
+const commandStr = 'npm run deploy';
+const child = require('child_process');
+
+async function watch(file) {
+
+	// get file type
+	const ext = path.extname(file);
+	// get file name
+	const name = path.basename(file, ext);
+
+	fs.watch(file, (event, filename) => {
+		if (filename) {
+			if (ext === '.js' || ext === '.sass' || ext === '.scss' || ext === '.ts') {
+				console.log(`${filename} changed`);
+				command(commandStr)();
+			} else {
+				console.log(`${filename} changed`);
+				command("php ./tools/deploy.php")();
+			}
+		}
+
+	});
+}
+
+// generate a function that runs 'npm run deploy'
+watchDir(src)();
+
+
+function command(cmd) {
+	return () => {
+		console.log(`Running: ${cmd}`);
+		child.execSync(cmd);
+	};
+}
+
+function watchDir(dir) {
+	return () => {
+		fs.readdirSync(dir).forEach(file => {
+			const fullPath = path.resolve(dir, file);
+			if (fs.statSync(fullPath).isDirectory()) {
+				watchDir(fullPath)();
+			} else {
+				watch(fullPath);
+			}
+		});
+	};
+}
+
+watchDir(src)();
+
+console.log(`Node version: ${process.version}\n \n`);
+
+console.log('Watching...\n');
+console.log('Press Ctrl+C to exit.\n');

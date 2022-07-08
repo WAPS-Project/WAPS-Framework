27412a9d26d7ff954fd752374d0f761c75da74dc
Jonas Pfalzgraf <1200994-JosunLP@users.noreply.gitlab.com>
2022-07-08 15:08:34 +0000

    Bug/to many builds

--- tools/watcher.js
+++ tools/watcher.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 const src = './framework.src/';
-const commandStr = 'npm run deploy';
 const child = require('child_process');
 
-async function watch(file) {
+/**
+ *
+ * @param {string} file
+ */
+function watch(file) {
 
 	// get file type
 	const ext = path.extname(file);
@@ -12,34 +15,39 @@ async function watch(file) {
 	const name = path.basename(file, ext);
 
 	fs.watch(file, (event, filename) => {
+		console.log(event);
 		if (filename) {
-			if (ext === '.js' || ext === '.sass' || ext === '.scss' || ext === '.ts') {
+			if (ext === '.sass' || ext === '.scss' || ext === '.ts') {
 				console.log(`${filename} changed`);
-				command(commandStr)();
+				command("npm run deploy")();
 			} else {
 				console.log(`${filename} changed`);
-				command("php ./tools/deploy.php")();
+				command("node ./tools/deploy.js")();
 			}
 		}
-
 	});
 }
 
-// generate a function that runs 'npm run deploy'
-watchDir(src)();
-
-
+/**
+ *
+ * @param {string} cmd
+ * @returns void
+ */
 function command(cmd) {
 	return () => {
 		console.log(`Running: ${cmd}`);
-		let result = child.execSync(cmd);
-		console.log(result.toString());
+		console.log(child.execSync(cmd).toString());
 	};
 }
 
+/**
+ *
+ * @param {string} dir
+ * @returns void
+ */
 function watchDir(dir) {
 	return () => {
-		fs.readdirSync(dir).forEach(file => {
+		fs.readdirSync(dir, 'utf-8').forEach(file => {
 			const fullPath = path.resolve(dir, file);
 			if (fs.statSync(fullPath).isDirectory()) {
 				watchDir(fullPath)();
